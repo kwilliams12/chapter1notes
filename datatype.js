6dc81dd1dae1fb913292b37fc6a767e32764b05b
d5bf <d5bf@fresnou.org>
2023-02-06 10:00:30 -0800

    initial commit

---
+++ datatype.js
@@ -0,0 +1,33 @@
+console.log(420)
+//thisisacomment
+
+//members
+//integere
+console.log(-3)
+console.log(0)
+console.log(3)
+//floating point member
+console.log(0.14)
+console.log(100.045)
+//strings
+//characters, letters, symbol, spaces
+console.log("hello world")
+console.log("hello world!!")
+console.log("hello world 123")
+ //articmetic operator (match)
+console.log(14+2)
+console.log()
+console.log()
+console.log(14%2)
+console.log(15%10)
+
+//concatenation: combine strings
+console.log("my name e is ms.bolin")
+console.log('my'+' '+'age' +' is'+'* 15 *').
+console.log("your age is/n17.");
+
+//template literal - plug in a calculated value
+//use backticks '' next to the 1
+console.log("15 divided by 10 =${15/10} axcvzxcvx");
+
+
